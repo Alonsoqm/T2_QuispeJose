e61388fcaf0f262e108c3f693e6e7593dc7949a6
upn_i <upn_i@DESKTOP-1KG2GS2>
2024-05-27 22:43:09 -0500

    Agregar archivos de proyecto.

---
+++ T2_QuispeJose/Datos/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using T2_QuispeJose.Models;
+
+namespace T2_QuispeJose.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+        public DbSet<Distribuidor> Distribuidores { get; set; }
+    }
+}
+
---
+++ T2_QuispeJose/Models/Distribuidor.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace T2_QuispeJose.Models
+{
+    public class Distribuidor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string NombreDistribuidor { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string RazonSocial { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [Range(1900, 3000, ErrorMessage = "El año debe ser entre 1900 - 3000")]
+        public int AnioInicioOperacion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string Contacto { get; set; }
+    }
+}
